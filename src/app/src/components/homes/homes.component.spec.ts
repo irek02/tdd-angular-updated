b3aa1011c171b7ffa0b73744f42f9ed8d2044848
Irek Mirgaleev <imirgaleev@gmail.com>
2025-01-26 19:59:00 -0500

    homes spec

--- src/app/src/components/homes/homes.component.spec.ts
+++ src/app/src/components/homes/homes.component.spec.ts
@@ -4,7 +4,6 @@ import { provideHttpClient } from '@angular/common/http';
 import { DataService, Home } from '../../services/data.service';
 import { MatDialog } from '@angular/material/dialog';
 import { of } from 'rxjs';
-import { By } from '@angular/platform-browser';
 import { BookHomeComponent } from '../book-home/book-home.component';
 
 describe('HomesComponent', () => {
@@ -14,6 +13,7 @@ describe('HomesComponent', () => {
   let dialog: jasmine.SpyObj<MatDialog>;
 
   beforeEach(async () => {
+
     const dataServiceSpy = jasmine.createSpyObj('DataService', ['getHomes$']);
     const dialogSpy = jasmine.createSpyObj('MatDialog', ['open']);
 
@@ -31,43 +31,73 @@ describe('HomesComponent', () => {
     component = fixture.componentInstance;
     dataService = TestBed.inject(DataService) as jasmine.SpyObj<DataService>;
     dialog = TestBed.inject(MatDialog) as jasmine.SpyObj<MatDialog>;
-    fixture.detectChanges();
+
   });
 
   it('should display homes', () => {
+
     const mockHomes: Home[] = [
       { id: '1', title: 'Home 1', image: 'image1.jpg', location: 'Location 1', price: '100' },
       { id: '2', title: 'Home 2', image: 'image2.jpg', location: 'Location 2', price: '200' }
     ];
     dataService.getHomes$.and.returnValue(of(mockHomes));
 
-    component.ngOnInit();
     fixture.detectChanges();
 
-    const homeElements = fixture.debugElement.queryAll(By.css('[data-test="home"]'));
+    const homeElements = fixture.nativeElement.querySelectorAll('[data-test="home"]');
     expect(homeElements.length).toBe(2);
-    expect(homeElements[0].nativeElement.textContent).toContain('Home 1');
-    expect(homeElements[1].nativeElement.textContent).toContain('Home 2');
+
   });
 
-  it('should open dialog when Book Home button is clicked', () => {
-    const mockHome: Home = { id: '1', title: 'Home 1', image: 'image1.jpg', location: 'Location 1', price: '100' };
-    component.openDialog(mockHome);
+  it('should show home info', () => {
 
+    const mockHomes: Home[] = [
+      { id: '1', title: 'Home 1', image: 'image1.jpg', location: 'Location 1', price: '100' },
+    ];
+    dataService.getHomes$.and.returnValue(of(mockHomes));
+
+    fixture.detectChanges();
+
+    const home = fixture.nativeElement.querySelector('[data-test="home"]');
+
+    expect(home.querySelector('[data-test="image"]')).toBeTruthy();
+    expect(home.querySelector('[data-test="title"]').innerText).toEqual('Home 1');
+    expect(home.querySelector('[data-test="location"]').innerText).toEqual('Location 1');
+
+  });
+
+  it('should show Book button', () => {
+
+    const mockHomes: Home[] = [
+      { id: '1', title: 'Home 1', image: 'image1.jpg', location: 'Location 1', price: '100' },
+    ];
+    dataService.getHomes$.and.returnValue(of(mockHomes));
+    fixture.detectChanges();
+
+    const home = fixture.nativeElement.querySelector('[data-test="home"]');
+
+    expect(home.querySelector('[data-test="book-btn"]')).toBeTruthy();
+
+  });
+
+  it('should use dialog service to open a dialog when clicking on Book button', () => {
+
+    const mockHomes: Home[] = [
+      { id: '1', title: 'Home 1', image: 'image1.jpg', location: 'Location 1', price: '100' },
+    ];
+    dataService.getHomes$.and.returnValue(of(mockHomes));
+    fixture.detectChanges();
+
+    // grab the button to click
+    const bookBtn = fixture.nativeElement.querySelector('[data-test="book-btn"] button');
+    // click the button
+    bookBtn.click();
+    // assert that the dialog service was used to open a dialog
     expect(dialog.open).toHaveBeenCalledWith(BookHomeComponent, {
       width: '500px',
-      data: { home: mockHome }
+      data: { home: mockHomes[0] }
     });
-  });
 
-  it('should display no homes when getHomes$ returns an empty array', () => {
-    dataService.getHomes$.and.returnValue(of([]));
-
-    component.ngOnInit();
-    fixture.detectChanges();
-
-    const homeElements = fixture.debugElement.queryAll(By.css('.home'));
-    expect(homeElements.length).toBe(0);
   });
 
 });
