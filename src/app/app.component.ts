1ea6924e4b98476b1c1c3e440e896205ab4ca422
Irek Mirgaleev <imirgaleev@gmail.com>
2025-02-05 19:41:51 -0500

    no need for homes import

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './src/components/header/header.component';
-import { HomesComponent } from './src/components/homes/homes.component';
 
 @Component({
   selector: 'app-root',
   imports: [
     RouterOutlet,
     HeaderComponent,
-    HomesComponent,
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
