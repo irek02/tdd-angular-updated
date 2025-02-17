67d64d9b9fb452acc095cd4b03016d14adac91e6
Irek Mirgaleev <imirgaleev@gmail.com>
2025-02-17 07:07:58 -0500

    build the header section

--- src/app/src/components/header/header.component.spec.ts
+++ src/app/src/components/header/header.component.spec.ts
@@ -17,7 +17,23 @@ describe('HeaderComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('should show top part', () => {
+
+    expect(fixture.nativeElement.querySelector('[data-test="logo"')).toBeTruthy();
+    expect(fixture.nativeElement.querySelector('[data-test="search"')).toBeTruthy();
+    expect(fixture.nativeElement.querySelector('[data-test="menu"')).toBeTruthy();
+
   });
+
+  it('should show bottom part', () => {
+
+    expect(fixture.nativeElement.querySelector('[data-test="home-type"]')).toBeTruthy();
+    expect(fixture.nativeElement.querySelector('[data-test="dates"]')).toBeTruthy();
+    expect(fixture.nativeElement.querySelector('[data-test="guests"]')).toBeTruthy();
+    expect(fixture.nativeElement.querySelector('[data-test="price"]')).toBeTruthy();
+    expect(fixture.nativeElement.querySelector('[data-test="rooms"]')).toBeTruthy();
+    expect(fixture.nativeElement.querySelector('[data-test="amenities"]')).toBeTruthy();
+
+  });
+
 });
